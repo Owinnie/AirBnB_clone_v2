38db3ee8d739addaea2ba65c5221b16e2e0a4f80
Owinnie <99328720+Owinnie@users.noreply.github.com>
2022-09-12 06:20:13 +0300

    state

--- models/state.py
+++ models/state.py
@@ -14,10 +14,9 @@ class State(BaseModel, Base):
     """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
+    cities = relationship("City",  backref="state", cascade="delete")
 
-    if environ['HBNB_TYPE_STORAGE'] == 'db':
-        cities = relationship('City', cascade='all, delete', backref='state')
-    else:
+    if environ['HBNB_TYPE_STORAGE'] != 'db':
         @property
         def cities(self):
             """Getter method for cities
