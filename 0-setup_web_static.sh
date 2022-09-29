0ea6d063f760ded8371a2509c96748cd645955fb
Owinnie <99328720+Owinnie@users.noreply.github.com>
2022-09-29 13:27:40 +0300

    change group permissions

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env bash
-# Set up server file system for deployment
+# Setting up web servers for deployment
 
-# install nginx
+# installing nginx
 sudo apt-get -y update
 sudo apt-get -y install nginx
 sudo service nginx start
 
-# configure file system
+# configure File System
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
 echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html > /dev/null
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-# set permissions
+# setting permissions
 sudo chown -R ubuntu:ubuntu /data/
+sudo chgrp -R ubuntu:ubuntu /data/
 
 # configure nginx
 sudo sed -i '44i \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
